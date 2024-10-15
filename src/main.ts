14556ba52454cfbcc081bd33b9f4fb6a4a1b521d
Sakarias Näslund <sakarias.naslund@gmail.com>
2024-10-15 21:49:56 +0200

    Added localStorage functionality

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,14 @@ searchForm?.addEventListener("submit", (e) => {
   fetchWeatherData(city);
 });
 
+const lastWeatherDisplayed: IWeatherData = JSON.parse(
+  localStorage.getItem("lastWeatherDisplayed") || "[]"
+);
+
+if (lastWeatherDisplayed) {
+  createHtml(lastWeatherDisplayed);
+}
+
 function fetchWeatherData(city: string) {
   fetch(
     "http://api.weatherapi.com/v1/current.json?key=758823ff1cbd4d63923142731241510&q=" +
@@ -17,25 +25,32 @@ function fetchWeatherData(city: string) {
   )
     .then((response) => response.json())
     .then((result: IWeatherData) => {
-      const app: HTMLElement | null = document.getElementById("app");
-      const weatherInfo: HTMLElement | null =
-        document.getElementById("weatherInfo");
+      createHtml(result);
 
-      // Töm tidigare väderinformation:
-      if (weatherInfo) {
-        weatherInfo.innerHTML = "";
-      }
-      const weatherCard = document.createElement("div");
-      weatherCard.className = "weather-card";
-      weatherCard.innerHTML = `
-        <h2>Stad: ${result.location.name}<h2>
-        <p>Temperatur: ${result.current.temp_c}°C</p>
-        <p>Känns som: ${result.current.feelslike_c}°C</p>
-      `;
-
-      weatherInfo?.appendChild(weatherCard);
-      if (app && weatherInfo) {
-        app.appendChild(weatherInfo);
-      }
+      localStorage.setItem("lastWeatherDisplayed", JSON.stringify(result));
     });
+  createHtml(lastWeatherDisplayed);
+}
+
+function createHtml(weatherData: IWeatherData) {
+  const app: HTMLElement | null = document.getElementById("app");
+  const weatherInfo: HTMLElement | null =
+    document.getElementById("weatherInfo");
+
+  // Töm tidigare väderinformation:
+  if (weatherInfo) {
+    weatherInfo.innerHTML = "";
+  }
+  const weatherCard = document.createElement("div");
+  weatherCard.className = "weather-card";
+  weatherCard.innerHTML = `
+      <h2>Stad: ${weatherData.location.name}<h2>
+      <p>Temperatur: ${weatherData.current.temp_c}°C</p>
+      <p>Känns som: ${weatherData.current.feelslike_c}°C</p>
+    `;
+
+  weatherInfo?.appendChild(weatherCard);
+  if (app && weatherInfo) {
+    app.appendChild(weatherInfo);
+  }
 }
